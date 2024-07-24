e7c53791f9902e2f9cb0689d12204c56f0176138
Isaac Campos <isaac.campos.ard@gmail.com>
2024-07-24 21:29:46 +0200

    update changes

--- 2024/Civilizaciones/solucion.py
+++ 2024/Civilizaciones/solucion.py
@@ -1,13 +1,13 @@
 t = int(input())
 for _ in range(t):
     n = int(input())
-    A = list(map(int, input().split()))
-    B = [100/A[i] if A[i]>100 else 1 for i in range(n)]
+    arreglo_de_eventos = list(map(int, input().split()))
+    arreglo_de_multiplicaciones_acumuladas = [100/arreglo_de_eventos[i] if arreglo_de_eventos[i]>100 else 1 for i in range(n)]
     for i in range(n):
-        if i !=0: B[i] = B[i]*B[i-1]
-    B = [1] + B   
-    print(B)
+        if i !=0: arreglo_de_multiplicaciones_acumuladas[i] = arreglo_de_multiplicaciones_acumuladas[i]*arreglo_de_multiplicaciones_acumuladas[i-1]
+    arreglo_de_multiplicaciones_acumuladas = [1] + arreglo_de_multiplicaciones_acumuladas   
+    print(arreglo_de_multiplicaciones_acumuladas)
     q = int(input())
     for qi in range(q):
         x,y,p = map(int, input().split())
-        print("S" if 100*(B[x]/B[y]) < p else "N")
+        print("S" if 100*(arreglo_de_multiplicaciones_acumuladas[x]/arreglo_de_multiplicaciones_acumuladas[y]) >= p else "N")
