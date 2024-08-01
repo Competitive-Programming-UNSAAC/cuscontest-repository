3eddf613e58ca2bee89c25923709d562cd5178f7
Dennis Huillca <dennisbot@gmail.com>
2024-08-01 02:51:20 -0500

    problema: numeros de serie

--- 2024/NumeroDeSerie/solution.cpp
+++ 2024/NumeroDeSerie/solution.cpp
@@ -11,13 +11,6 @@
 typedef long long ll;
 #include <limits>
 #include <memory>
-#define db(a) cout << #a << " = " << a << endl;
-#define db2(a, b) cout << #a << " = " << a << " " << #b << " = " << b << endl;
-#define db3(a, b, c) cout << #a << " = " << a << " " << #b << " = " << b << " " << #c << " = " << c << endl;
-#define list(a)                                                                         \
-    cout << #a << endl;                                                                 \
-    for (size_t i = 0; i < (a).size(); ++i) cout << "[" << i << "]:" << (a)[i] << endl; \
-    cout << endl;
 
 using namespace std;
 
