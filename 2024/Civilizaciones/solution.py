4df8cda18c9aeb8a809c3178ce1231fedd15bb2e
Isaac Campos <isaac.campos.ard@gmail.com>
2024-08-01 20:28:25 +0200

    update code and test cases

--- 2024/Civilizaciones/solution.py
+++ 2024/Civilizaciones/solution.py
@@ -1,14 +1,16 @@
-with open("./in/1.in",'r') as inp:
+YES = "S"
+NO = "N"
 
-    t = int(inp.readline())
-    for _ in range(t):
-        n = int(inp.readline())
-        arreglo_de_eventos = list(map(int, inp.readline().split()))
-        arreglo_de_multiplicaciones_acumuladas = [100/arreglo_de_eventos[i] if arreglo_de_eventos[i]>100 else 1 for i in range(n)]
-        for i in range(n):
-            if i !=0: arreglo_de_multiplicaciones_acumuladas[i] = arreglo_de_multiplicaciones_acumuladas[i]*arreglo_de_multiplicaciones_acumuladas[i-1]
-        arreglo_de_multiplicaciones_acumuladas = [1] + arreglo_de_multiplicaciones_acumuladas   
-        q = int(inp.readline())
-        for qi in range(q):
-            x,y,p = map(int, inp.readline().split())
-            print("S" if 100*(arreglo_de_multiplicaciones_acumuladas[x]/arreglo_de_multiplicaciones_acumuladas[y]) >= p else "N")
+for _ in range(int(input())):
+    n = int(input())
+    eventos = list(map(int, input().split()))
+
+    acumulado = [100 / eventos[i] if eventos[i] > 100 else 1 for i in range(n)]
+    for i in range(n):
+        if i !=0:
+            acumulado[i] = acumulado[i] * acumulado[i - 1]
+    acumulado = [1] + acumulado
+
+    for _ in range(int(input())):
+        x, y, p = map(int, input().split())
+        print(YES if 100 * (acumulado[x] / acumulado[y]) >= p else NO)
